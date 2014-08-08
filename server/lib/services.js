4437e3db5faad49d28ea0777e2440db3119ea2d7
Jimmy Garzon <jimmygarzon@gmail.com>
2014-08-07 21:18:08 -0500

    initial commit

---
+++ server/lib/services.js
@@ -0,0 +1,8 @@
+ServiceConfiguration.configurations.remove({});
+
+ServiceConfiguration.configurations.insert({
+  "service" : "twitter",
+  "consumerKey" : "your API key here",
+  "secret" : "your API secret here"
+});
+
